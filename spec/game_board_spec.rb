4448d388195c25bbd1f990fd21b35d8c84b123d6
John Moon <johnmoonyy@gmail.com>
2014-03-27 17:18:18 -0400

    Add feature of adding pieces to existing board

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -17,4 +17,14 @@ describe GameBoard do
                          [4, 5, 6],
                          [7, 8, 9]])
   end
+
+  it 'displays new board after pieces are placed' do
+    ttt = TTTGame.new
+    ttt.pieces_played(1)
+    ttt.pieces_played(3)
+    piece = ttt.place_piece
+    expect(piece).to eq([['o', 2, 'x'],
+                         [4, 5, 6],
+                         [7, 8, 9]])
+  end
 end
