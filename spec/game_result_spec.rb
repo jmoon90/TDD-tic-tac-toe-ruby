db55a29e172cdb3c49d3b379ce7e3d0a0b8f0d34
John Moon <johnmoonyy@gmail.com>
2014-03-28 18:07:46 -0400

    the game should not output a result if game isn't over

--- spec/game_result_spec.rb
+++ spec/game_result_spec.rb
@@ -13,4 +13,13 @@ describe GameResult do
     total_hands = [1,2,3,4,5,6,7,8,9]
     expect(gr.output(computer_hand, total_hands)).to eql('Tied game')
   end
+
+  it "doesn't do anything if game isn't over" do
+    gr = GameResult.new
+    player_hand = [8]
+    computer_hand = [1,3]
+    total_hands = [1,3,8]
+
+    expect(gr.output(computer_hand, total_hands)).to eql(nil)
+  end
 end
