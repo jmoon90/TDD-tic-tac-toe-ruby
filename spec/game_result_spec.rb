fa83d3c710b766b792b4d8546c587affe6ce3cf0
John Moon <johnmoonyy@gmail.com>
2014-03-29 10:58:19 -0400

    Pending game result for later

--- spec/game_result_spec.rb
+++ spec/game_result_spec.rb
@@ -1,6 +1,7 @@
 require 'game_result'
 describe GameResult do
   it 'determines if computer won' do
+    pending "pending"
     gr = GameResult.new
     computer_hand = [1,2,3]
     total_hands = [1,2,3,8,9]
@@ -8,6 +9,7 @@ describe GameResult do
   end
 
   it 'determines if game was tied' do
+    pending "pending"
     gr = GameResult.new
     computer_hand = [1,3,4,9]
     total_hands = [1,2,3,4,5,6,7,8,9]
@@ -15,6 +17,7 @@ describe GameResult do
   end
 
   it "doesn't do anything if game isn't over" do
+    pending "pending"
     gr = GameResult.new
     player_hand = [8]
     computer_hand = [1,3]
